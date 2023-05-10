3da1eb40483c397af7685b4b65b1444d90eddac6
Juan Montoya <juanmontoya015@gmail.com>
2023-05-09 21:44:20 -0500

    Initial commit

---
+++ src/test/java/com/workshop/millenio/MillenioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workshop.millenio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MillenioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
